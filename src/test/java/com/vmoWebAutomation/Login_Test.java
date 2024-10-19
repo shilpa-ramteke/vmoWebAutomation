1bc05081bb798c2d53ae7a190c8dbdf8675a1d29
shilpabor <shilpaborkar294@gamil.com>
2024-10-19 17:23:56 +0530

    Commit 19Oct 2nd Task

--- src/test/java/com/vmoWebAutomation/Login_Test.java
+++ src/test/java/com/vmoWebAutomation/Login_Test.java
@@ -1,2 +1,44 @@
-package com.vmoWebAutomation;public class Login_Page {
+package com.vmoWebAutomation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Login_Test {
+
+
+    @Test
+    public void loginWithValidCredential() {
+        WebDriver driver=new EdgeDriver();
+        driver.get("https://app.vwo.com");
+
+        WebElement UserName=driver.findElement(By.id("login-username"));
+        UserName.sendKeys("shilpa@gmail.com");
+
+        WebElement passWord=driver.findElement(By.id("login-password"));
+        passWord.sendKeys("Test@12345");
+
+       WebElement signInButton=driver.findElement(By.id("js-login-btn"));
+        signInButton.click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement dashboradVarification=driver.findElement(By.xpath("//*[@class='navbar-btn-label']"));
+         String actualResult=dashboradVarification.getText();
+         if (actualResult.equals("Dashboard"))
+         {
+             Assert.assertTrue(true);
+         }
+
+          driver.quit();
+    }
 }
